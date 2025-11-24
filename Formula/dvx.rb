a05359c437d8695da70fb2bf1382de281b2d186a
Jonas Marquez <jonas@jonasmarquez.com>
2025-11-24 22:10:08 +0100

    feat: add Homebrew formula for dvx 0.12.0

--- Formula/dvx.rb
+++ Formula/dvx.rb
@@ -6,14 +6,20 @@ class Dvx < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.12.0/dvx_0.12.0_darwin_arm64.tar.gz"
-      sha256 "d32950c9947d213155d82e2f079a4ff72f979c95ee309a13a0184523aed1e544"
+      sha256 "2f184801bd52bbcb9502b7933bf1fc2afd519bf336911cea51bef79d5dbfd5de"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.12.0/dvx_0.12.0_darwin_amd64.tar.gz"
+      sha256 "fdaad6c7833e929ebd3e32309caeb89ce3ad0d3f314b1ad55ed9c7370c0ab8b2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.12.0/dvx_0.12.0_linux_amd64.tar.gz"
-      sha256 "7e717ed312718a7e66f1218e7a6206d94c6494cde5779393ac495dbf9a2fefb3"
+      sha256 "ba5df88aa2ec57234ba9f15d6a3141ad1a2a89c68099ec0577abbf21a09315de"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.12.0/dvx_0.12.0_linux_arm64.tar.gz"
+      sha256 "d1a2d5d65cdf826f5fc9e0a36597b218a35e0343ae0c28ada3ef247ad66dfb2a"
     end
   end
 
