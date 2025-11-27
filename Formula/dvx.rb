9eedf01bd07e30af1f1191102b63948f5c1c1ed8
Jonas Marquez <jonas@jonasmarquez.com>
2025-11-27 19:40:19 +0100

    feat: add Homebrew formula for dvx 0.14.0

--- Formula/dvx.rb
+++ Formula/dvx.rb
@@ -1,25 +1,25 @@
 class Dvx < Formula
   desc "Dev eXperience helper CLI to manage dimensions, envs, Kubernetes and secrets"
   homepage "https://github.com/jonasmarquez/dvx-bin"
-  version "0.13.0"
+  version "0.14.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.13.0/dvx_0.13.0_darwin_arm64.tar.gz"
-      sha256 "3cfc9669af85f25ca26328335ca7b66c9da9c8b84053338e6bd7683526424f1d"
+      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.14.0/dvx_0.14.0_darwin_arm64.tar.gz"
+      sha256 "47c3e53932c3853a281658ae2281bf7035332fd23cc104690f28b689ddcd64f9"
     elsif Hardware::CPU.intel?
-      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.13.0/dvx_0.13.0_darwin_amd64.tar.gz"
-      sha256 "f0e63180324a963cb786b298b82fb572529d61c6aab09ad47d7be362f075e090"
+      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.14.0/dvx_0.14.0_darwin_amd64.tar.gz"
+      sha256 "2cd0aa469eafa143c04b3ac454ab62ff80d2b9770c7fab2c7f91ffecfd0e8834"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.13.0/dvx_0.13.0_linux_amd64.tar.gz"
-      sha256 "79589477323c1345bfd34ce02f59edb33da59464dfc88e3e0c25e3e749b4be0d"
+      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.14.0/dvx_0.14.0_linux_amd64.tar.gz"
+      sha256 "d83d2e0ccaca61e7d4be2247222a7bc364dcf93f569ff01589a8ed0b94b771a5"
     elsif Hardware::CPU.arm?
-      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.13.0/dvx_0.13.0_linux_arm64.tar.gz"
-      sha256 "0bcecbff88a6a0c315dc4951d6b0e09ab6e5a4f931160285f2ddef708af37172"
+      url "https://github.com/jonasmarquez/dvx-bin/releases/download/v0.14.0/dvx_0.14.0_linux_arm64.tar.gz"
+      sha256 "6133af224c43288092e1bc59e168114ef0661f139fbc548f1d8117c6ae5b2bcf"
     end
   end
 
@@ -29,6 +29,6 @@ class Dvx < Formula
 
   test do
     output = shell_output("#{bin}/dvx version")
-    assert_match "dvx ⚙️  version 0.13.0", output
+    assert_match "dvx ⚙️  version 0.14.0", output
   end
 end
